f76d9b3f16e4bbeb77a5edb806d431bfaadf593c
Alexandre Belley <ab@mambomambo.ca>
2023-04-20 11:27:44 -0400

    tests

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
     <p>App</p>
+    <CollapsibleBase>
+        <template #headerText>Ouvrir</template>
+        <template #content>
+            <p>Lorem ipsum dolor sit amet</p>
+        </template>
+    </CollapsibleBase>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CollapsibleBase from "./components/CollapsibleBase";
+</script>
 
 <style lang="scss" scoped></style>
