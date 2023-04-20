a4e03788745974013232baad1077455c4d0754f3
Alexandre Belley <ab@mambomambo.ca>
2023-04-20 12:15:29 -0400

    Base structure

---
+++ src/components/ScrollableBase/ScrollableBase.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="queso-scrollable" :class="scrollableClasses">
+        <div ref="scrollableContent" class="queso-scrollable__content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+// import { computed, ref, onBeforeMount } from "vue";
+// import { useElementBounding } from "@vueuse/core";
+</script>
+
+<style lang="scss">
+.queso-scrollable {
+}
+</style>
