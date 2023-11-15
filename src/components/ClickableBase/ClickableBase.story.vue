eb6c1e851cdcfd2ca376b2ed853e1e928c40c0e1
Alexandre Belley <ab@mambomambo.ca>
2023-11-15 15:18:52 -0500

    ClickableBase Story

---
+++ src/components/ClickableBase/ClickableBase.story.vue
@@ -0,0 +1,20 @@
+<template>
+    <Story title="Clickable" :init-state="initState" icon="mdi:button-pointer">
+        <template #default="{ state }">
+            <clickable-base :is-disabled="state.isDisabled">
+                {{ state.label }}
+            </clickable-base>
+        </template>
+    </Story>
+</template>
+
+<script setup lang="ts">
+import ClickableBase from "./ClickableBase.vue";
+
+const initState = () => {
+    return {
+        isDisabled: true,
+        label: "Default button",
+    };
+};
+</script>
