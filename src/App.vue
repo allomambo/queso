eb88e71eb4199724a370b4fb44f956912e426ca9
AurelienVigne <yay@aurelienvigne.com>
2023-11-16 14:47:58 -0500

    clean up

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,7 @@
 <template>
     <p>App</p>
-    <app-select :options="mockedSelect">
-        <template #label>test</template>
-    </app-select>
 </template>
 
-<script setup lang="ts">
-import AppSelect from "@components/AppSelect";
-
-const mockedSelect = [
-    {
-        key: 1,
-        data: { label: "Option 1" },
-    },
-
-    {
-        key: 2,
-        data: { label: "Option 2" },
-    },
-];
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss" scoped></style>
