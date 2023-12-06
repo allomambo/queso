80910d0851e4df819e574de2fb34046a04ab22db
AurelienVigne <yay@aurelienvigne.com>
2023-12-06 10:37:59 -0500

    added some test to open checkbox

--- src/components/QuesoCheckbox/QuesoCheckbox.test.ts
+++ src/components/QuesoCheckbox/QuesoCheckbox.test.ts
@@ -13,4 +13,26 @@ describe("QuesoCheckbox", () => {
         });
         expect(wrapper.vm).toBeTruthy();
     });
+
+    test("renders correctly the label", () => {
+        const data = {
+            label: "field-label",
+        };
+
+        const wrapper = mount(QuesoCheckbox, {
+            propsData: data,
+        });
+        expect(wrapper.find(".queso-checkbox__label__text").text()).toBe(data.label);
+    });
+
+    test("renders correctly the checkbox symbol", () => {
+        const data = {
+            name: "field-name",
+        };
+
+        const wrapper = mount(QuesoCheckbox, {
+            propsData: data,
+        });
+        expect(wrapper.find(".queso-checkbox__box__symbol").exists()).toBe(true);
+    });
 });
