f3bb879ac3bab9d26259abe1b8ed36c24078f629
Alexandre Belley <ab@mambomambo.ca>
2023-12-06 16:29:20 -0500

    Fix some configs

--- histoire.config.ts
+++ histoire.config.ts
@@ -3,4 +3,9 @@ import { HstVue } from "@histoire/plugin-vue";
 
 export default defineConfig({
     plugins: [HstVue()],
+    vite: {
+        resolve: {
+            mainFields: ["module", "jsnext:main", "jsnext"],
+        },
+    },
 });
