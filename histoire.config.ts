0f25ad594c9423ac020e213ab015f5b6820c6e1c
Alexandre Belley <ab@mambomambo.ca>
2023-12-06 16:52:16 -0500

    Testing to replace vite build config

--- histoire.config.ts
+++ histoire.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "histoire";
 import { HstVue } from "@histoire/plugin-vue";
 
+import vue from "@vitejs/plugin-vue";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+
 export default defineConfig({
     plugins: [HstVue()],
     vite: {
+        build: {
+            cssCodeSplit: true,
+        },
         resolve: {
             mainFields: ["module", "jsnext:main", "jsnext"],
         },
+        plugins: [vue(), libInjectCss()],
     },
 });
