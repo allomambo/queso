faefe8cfcd64f3bc0f17e3b7a46e8e25280a5e80
Alexandre Belley <ab@mambomambo.ca>
2024-05-11 17:56:01 -0400

    Upgrade TextArea

--- src/components/QuesoTextArea/QuesoTextArea.vue
+++ src/components/QuesoTextArea/QuesoTextArea.vue
@@ -1,63 +1,79 @@
 <template>
-    <queso-field v-bind="$props">
-        <template #label="fieldProps">
-            <slot name="label" v-bind="{ ...fieldProps }"></slot>
+    <queso-field class="-text-area" v-bind="extendedProps">
+        <template #beforeLabel>
+            <slot name="beforeLabel"></slot>
+        </template>
+        <template #label="exposedData">
+            <slot name="label" v-bind="{ ...exposedData }"></slot>
+        </template>
+        <template #required="exposedData">
+            <slot name="required" v-bind="{ ...exposedData }"></slot>
+        </template>
+        <template #afterLabel>
+            <slot name="afterLabel"></slot>
         </template>
 
         <template #beforeInput>
             <slot name="beforeInput"></slot>
         </template>
-
         <template
             #input="{
                 fieldID,
                 fieldName,
                 fieldValue,
-                updateValue,
-                toggleIsActive,
-                toggleIsHover,
                 isRequired,
                 isDisabled,
                 isReadOnly,
+                toggleIsActive,
+                toggleIsHover,
             }"
         >
             <div class="queso-text-area">
+                <slot name="beforeTextAreaInput"></slot>
+
                 <span v-if="isReadOnly" class="queso-text-area__readonly" v-html="fieldValue"></span>
 
                 <textarea
                     v-else
-                    :name="fieldName"
-                    :id="fieldID"
-                    :value="fieldValue"
                     class="queso-text-area__input"
+                    :id="fieldID"
+                    :name="fieldName"
                     :placeholder="placeholder"
                     :required="isRequired"
                     :disabled="isDisabled"
-                    @input="updateValue"
                     @mouseover="toggleIsHover(true)"
                     @mouseleave="toggleIsHover(false)"
                     @focus="toggleIsActive(true)"
                     @blur="toggleIsActive(false)"
+                    v-bind="extraAttributes"
+                    v-model="model"
                 ></textarea>
+
+                <slot name="afterTextAreaInput"></slot>
             </div>
         </template>
-
         <template #afterInput>
             <slot name="afterInput"></slot>
         </template>
 
-        <template #error="fieldProps">
-            <slot name="error" v-bind="{ ...fieldProps }"></slot>
+        <template #error="exposedData">
+            <slot name="error" v-bind="{ ...exposedData }"></slot>
         </template>
     </queso-field>
 </template>
 
 <script setup lang="ts">
+import { useExtendedFieldProps } from "@composables/fields";
+
 import type { QuesoTextAreaProps } from "./types";
 
 import QuesoField from "@components/QuesoField";
 
 const props = defineProps<QuesoTextAreaProps>();
+
+const model = defineModel<string>({ required: true, default: "" });
+
+const extendedProps = useExtendedFieldProps(props);
 </script>
 
 <style lang="scss">
