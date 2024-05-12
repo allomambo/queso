0a999240ed888e2c7cc2fb65384eca9c6b686a33
Alexandre Belley <ab@mambomambo.ca>
2024-05-11 23:29:36 -0400

    Revert TextField test

--- src/components/QuesoTextField/QuesoTextField.test.ts
+++ src/components/QuesoTextField/QuesoTextField.test.ts
@@ -1,27 +1,38 @@
-import { mount } from "@vue/test-utils";
-import { expect, test } from "vitest";
-
+import { mount, shallowMount } from "@vue/test-utils";
+import { describe, expect, test } from "vitest";
 import QuesoTextField from "./QuesoTextField.vue";
 
-function createComponent(propsData) {
-    return mount(QuesoTextField, { propsData });
-}
+describe("QuesoTextField", () => {
+    test("renders correctly the object", () => {
+        const data = {
+            name: "field-name",
+        };
 
-test("QuesoTextField.vue", async () => {
-    // const wrapper = createComponent({ type: "text", modelValue: "test" });
-    // expect(wrapper.vm).toBeTruthy();
-    // expect(wrapper.html()).toMatchSnapshot();
-    // const input = wrapper.find("input");
-    // expect(input.exists()).toBe(true);
-    // await input.setValue("testing");
-    // expect(wrapper.props("modelValue")).toEqual("testing");
-    // const wrapperWithSlots = mount(QuesoTextField, {
-    //     props: { label: "Label" },
-    //     slots: {
-    //         beforeLabel: "<div>Before Label</div>",
-    //         afterLabel: "<div>After Label</div>",
-    //     },
-    // });
-    // expect(wrapperWithSlots.html()).toContain("Before Label");
-    // expect(wrapperWithSlots.html()).toContain("After Label");
+        const wrapper = shallowMount(QuesoTextField, {
+            props: data,
+        });
+        expect(wrapper.vm).toBeTruthy();
+    });
+
+    test("renders correctly the label", () => {
+        const data = {
+            label: "field-label",
+        };
+
+        const wrapper = mount(QuesoTextField, {
+            props: data,
+        });
+        expect(wrapper.find(".queso-field__label").text()).toBe(data.label);
+    });
+
+    test("renders correctly the input", () => {
+        const data = {
+            name: "field-name",
+        };
+
+        const wrapper = mount(QuesoTextField, {
+            props: data,
+        });
+        expect(wrapper.find(".queso-field__input").exists()).toBe(true);
+    });
 });
