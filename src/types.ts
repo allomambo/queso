a7d37e8cbaebeb003cd8e988185c4b1d09ef8141
Alexandre Belley <ab@mambomambo.ca>
2024-05-13 15:48:16 -0400

    Types file

---
+++ src/types.ts
@@ -0,0 +1,30 @@
+// Bases
+import type { QuesoClickableMarkup, QuesoClickableProps } from "@components/QuesoClickable";
+import type { QuesoCollapsibleProps } from "@components/QuesoCollapsible";
+import type { QuesoIconSizeUnit, QuesoIconProps } from "@components/QuesoIcon";
+import type { QuesoModalOpen, QuesoModalClose, QuesoModalMethods } from "@components/QuesoModal";
+import type { QuesoScrollableProps } from "@components/QuesoScrollable";
+
+// Fields
+// Coming after PR #132 is merged
+
+/*====================================
+=              EXPORTS               =
+====================================*/
+
+export {
+    // Clickable
+    QuesoClickableMarkup,
+    QuesoClickableProps,
+    // Collapsible
+    QuesoCollapsibleProps,
+    // Icon
+    QuesoIconSizeUnit,
+    QuesoIconProps,
+    // Modal
+    QuesoModalOpen,
+    QuesoModalClose,
+    QuesoModalMethods,
+    // Scrollable
+    QuesoScrollableProps,
+};
