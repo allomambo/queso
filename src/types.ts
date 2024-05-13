7d3bae07bc5150b4cee7b60c9b2b5cd7268c05d8
Alexandre Belley <ab@mambomambo.ca>
2024-05-13 16:27:04 -0400

    Add fields to types

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,42 @@
 // Bases
 import type { QuesoClickableMarkup, QuesoClickableProps } from "@components/QuesoClickable";
 import type { QuesoCollapsibleProps } from "@components/QuesoCollapsible";
+import type {
+    QuesoDropdownOptionValue,
+    QuesoDropdownOptionValues,
+    QuesoDropdownOption,
+    QuesoDropdownOptions,
+    QuesoDropdownProps,
+} from "@components/QuesoDropdown";
 import type { QuesoIconSizeUnit, QuesoIconProps } from "@components/QuesoIcon";
 import type { QuesoModalOpen, QuesoModalClose, QuesoModalMethods } from "@components/QuesoModal";
 import type { QuesoScrollableProps } from "@components/QuesoScrollable";
 
 // Fields
-// Coming after PR #132 is merged
+import type { QuesoFieldBaseExtraAttributes, QuesoFieldBase, QuesoFieldProps } from "@components/QuesoField";
+import type { QuesoTextFieldType, QuesoTextFieldProps } from "@components/QuesoTextField";
+import type { QuesoTextAreaProps } from "@components/QuesoTextArea";
+import type { QuesoCheckboxProps } from "@components/QuesoCheckbox";
+import type { QuesoSelectProps } from "@components/QuesoSelect";
 
 /*====================================
 =              EXPORTS               =
 ====================================*/
 
-export {
+export type {
+    // -- Bases -- //
+
     // Clickable
     QuesoClickableMarkup,
     QuesoClickableProps,
     // Collapsible
     QuesoCollapsibleProps,
+    // Dropdown
+    QuesoDropdownOptionValue,
+    QuesoDropdownOptionValues,
+    QuesoDropdownOption,
+    QuesoDropdownOptions,
+    QuesoDropdownProps,
     // Icon
     QuesoIconSizeUnit,
     QuesoIconProps,
@@ -27,4 +46,20 @@ export {
     QuesoModalMethods,
     // Scrollable
     QuesoScrollableProps,
+
+    // -- Fields -- //
+
+    // Base
+    QuesoFieldBaseExtraAttributes,
+    QuesoFieldBase,
+    QuesoFieldProps,
+    // TextField
+    QuesoTextFieldType,
+    QuesoTextFieldProps,
+    // TextArea
+    QuesoTextAreaProps,
+    // Checkbox
+    QuesoCheckboxProps,
+    // Select
+    QuesoSelectProps,
 };
