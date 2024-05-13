65ff10a809afc2463947b85f04b10969a0bc1bc0
Alexandre Belley <ab@mambomambo.ca>
2024-05-13 14:25:36 -0400

    Use v-bind for anchor attributes

--- src/components/QuesoClickable/QuesoClickable.vue
+++ src/components/QuesoClickable/QuesoClickable.vue
@@ -3,12 +3,9 @@
         :is="markup"
         class="queso-clickable"
         :class="clickableClasses"
-        :href="clickableHref"
-        :to="clickableTo"
-        :target="clickableTarget"
-        :download="clickableDownload"
         :disabled="clickableDisabled"
         :aria-disabled="clickableDisabled"
+        v-bind="anchorAttributes"
     >
         <slot></slot>
     </component>
@@ -26,14 +23,30 @@ const props = withDefaults(defineProps<QuesoClickableProps>(), {
 const { isExternal, isDisabled, isDownload, markup, url } = toRefs(props);
 
 const isAnchor = computed<boolean>(() => markup.value === "a");
-const isRouter = computed<boolean>(() => markup.value === "router-link");
+const isRouter = computed<boolean>(() => markup.value === "router-link" && typeof url.value === "object");
+
+// Attributes when anchor and router-link tags
+const anchorAttributes = computed(() => {
+    let attrs: object = {};
+
+    if (isRouter.value) {
+        attrs = {
+            to: url.value,
+        };
+    } else if (isAnchor.value) {
+        attrs = {
+            href: url.value,
+            target: clickableTarget.value,
+            download: clickableDownload.value,
+        };
+    }
+
+    return attrs;
+});
 
 // Computeds
-const clickableHref = computed(() => (isAnchor.value || isRouter.value ? url.value : null));
-const clickableTo = computed(() => (isRouter.value ? url.value : null));
 const clickableTarget = computed(() => (isAnchor.value && isExternal.value ? "_blank" : null));
-
-const clickableDisabled = computed(() => (isDisabled.value ? true : null));
+const clickableDisabled = computed(() => (isDisabled.value ? "" : null));
 const clickableDownload = computed(() => (isAnchor.value && isDownload.value ? "" : null));
 
 // CSS classes
@@ -45,7 +58,6 @@ const clickableClasses = computed(() => ({
 
 <style lang="scss">
 .queso-clickable {
-    //--- State: Disabled ---//
     &.is-disabled {
         @include unselectable;
     }
