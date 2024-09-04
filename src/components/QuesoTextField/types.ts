9c8b5e60ebcbe67699e65ff4320d49a91e82ae85
Antoine Savard <antoinesavard8@gmail.com>
2024-09-04 11:53:17 -0400

    Remove "password" option from QuesoTextFieldType

--- src/components/QuesoTextField/types.ts
+++ src/components/QuesoTextField/types.ts
@@ -1,6 +1,6 @@
 import type { QuesoFieldBase, QuesoFieldProps } from "@components/QuesoField";
 
-export type QuesoTextFieldType = "text" | "url" | "tel" | "email" | "password";
+export type QuesoTextFieldType = "text" | "url" | "tel" | "email";
 export type QuesoTextFieldModel = string;
 
 export interface QuesoTextFieldProps extends QuesoFieldProps, QuesoFieldBase {
