a7d311517104be418c12ad9336ff16277628395b
Antoine Savard <antoinesavard8@gmail.com>
2024-09-04 15:05:05 -0400

    Update dependencies and add QuesoModal focus trap functionality

--- src/components/QuesoModal/QuesoModal.vue
+++ src/components/QuesoModal/QuesoModal.vue
@@ -1,6 +1,12 @@
 <template>
     <Teleport to="body">
-        <div class="queso-modal" :class="{ 'is-modal-open': isModalOpen }" :aria-expanded="isModalOpen" v-bind="$attrs">
+        <div
+            class="queso-modal"
+            :class="{ 'is-modal-open': isModalOpen }"
+            :aria-expanded="isModalOpen"
+            v-bind="$attrs"
+            ref="target"
+        >
             <slot name="beforeContent"></slot>
 
             <div class="queso-modal__content">
@@ -18,6 +24,7 @@
 
 <script setup lang="ts">
 import { ref, watch, onMounted, provide } from "vue";
+import { useFocusTrap } from "@vueuse/integrations/useFocusTrap";
 
 import { QuesoModalMethodsKey } from "./types";
 import type { QuesoModalMethods, QuesoModalOpen, QuesoModalClose } from "./types";
@@ -25,6 +32,31 @@ import type { QuesoModalMethods, QuesoModalOpen, QuesoModalClose } from "./types
 import QuesoModalOverlay from "./components/QuesoModalOverlay";
 
 const emit = defineEmits(["modal:open", "modal:close"]);
+
+const target = ref();
+const containsFocusable = ref<boolean>(false);
+const { activate, deactivate } = useFocusTrap(target);
+
+// Active/Deactive Focus Trap
+// Active or deactivate focus trap only if the modal contains focusable elements
+const focusableSelectors = [
+    "a[href]",
+    "area[href]",
+    "input:not([disabled])",
+    "select:not([disabled])",
+    "textarea:not([disabled])",
+    "button:not([disabled])",
+    "iframe",
+    "object",
+    "embed",
+    '[tabindex]:not([tabindex="-1"])',
+    "[contenteditable]",
+];
+
+const hasFocusableElements = (element: HTMLElement | null): boolean => {
+    if (!element) return false;
+    return focusableSelectors.some((selector) => element.querySelector(selector) !== null);
+};
 
 // Open/Close modal
 const isModalOpen = ref<boolean>(false);
@@ -46,14 +78,25 @@ const toggleOverflowOnDocument = (bool: boolean = true) => {
 watch(isModalOpen, (isOpen) => {
     if (isOpen) {
         toggleOverflowOnDocument(true);
+
+        if (containsFocusable.value) {
+            activate();
+        }
+
         emit("modal:open");
     } else {
         toggleOverflowOnDocument(false);
+
+        if (containsFocusable.value) {
+            deactivate();
+        }
+
         emit("modal:close");
     }
 });
 
 onMounted(() => {
+    containsFocusable.value = hasFocusableElements(target.value);
     if (isModalOpen.value) {
         toggleOverflowOnDocument(true);
     }
